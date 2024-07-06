a613984e8e6e720702048c378535af16eea36a9e
Aviral Ale <69aviral@gmail.com>
2024-07-06 14:36:00 +0545

    Created basic layouts of pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, Routes } from "react-router-dom";
+import Layout from "./pages/Layout";
+import { ThemeProvider } from "./pages/components/ThemeProvider";
+import Register from "./pages/Auth/Register";
+import "./App.css";
+import { Login } from "./pages/Auth/Login";
+import PageNotFound from "./pages/Misc/404";
+import AccountActivation from "./pages/Auth/AccountActivation";
+import AccountActivated from "./pages/Auth/AccountActivated";
+import ForgotPassword from "./pages/Auth/ForgotPassword";
+import ResetPasswordConfirm from "./pages/Auth/ResetPasswordConfirm";
+import ForgotUsername from "./pages/Auth/ForgotUsername";
+import ResetUsernameConfirm from "./pages/Auth/ResetUsernameConfirm";
+import Dashboard from "./pages/user/Dashboard";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <Layout>
+        <Routes>
+          <Route path="/" element={<div>Home</div>} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/activate" element={<AccountActivation />} />
+          <Route path="/activated" element={<AccountActivated />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/forgot-username" element={<ForgotUsername />} />
+          <Route
+            path="/reset-password-confirm"
+            element={<ResetPasswordConfirm />}
+          />
+          <Route
+            path="/reset-username-confirm"
+            element={<ResetUsernameConfirm />}
+          />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/*" element={<PageNotFound />} />
+        </Routes>
+      </Layout>
+    </ThemeProvider>
+  );
 }
-
-export default App
